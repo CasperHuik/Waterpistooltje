4500ed372192e809c64a2e64aab40afa6b6231d4
Casper Huikeshoven <c.huikeshoven@student.tudelft.nl>
2024-03-21 21:31:23 +0100

    Added waterpistol

---
+++ Assets/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float startVelocity = 0f; 
+    float velocity = 0f;
+    public float gravity = 9.8f;
+    public Collider collider;
+    public Transform playerTranform;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = startVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+        transform.Translate(Vector3.down * gravity * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Ground")){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Gun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Gun : NetworkBehaviour
+{
+    public float damage = 0f;
+    public Transform player;
+    public Transform spawnPoint;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+            Fire();
+        }
+    }
+
+    void Fire(){
+        
+        Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+    }
+}
